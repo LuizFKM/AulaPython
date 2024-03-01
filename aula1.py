adecd998db82d9244d7a733ef12ea8b4f0a2f416
unknown <luizfk.marinho@gmail.com>
2024-03-01 20:57:42 -0300

    de novo

---
+++ aula1.py
@@ -0,0 +1,13 @@
+x = input("Digite o primeiro número: ")#entrada de dados
+y = input("Digite o segundo número: ")#entrada de dados
+
+x = int(x)#converte dado x em inteiro
+y = int(y)#converte dado y em inteiro
+
+# print(type(x), type(y))#mostra o tipo de dados
+# print (x + y)#soma os numeros
+
+#ou
+
+soma = x + y
+print("A soma é: ", soma)
