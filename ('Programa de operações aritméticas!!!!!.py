8b385b4002a9a1578753ab39229730f8631f6112
Luiz <luizfk.marinho@gmail.com>
2024-03-13 22:28:39 -0300

    Adicionando primeiros exercicios

---
+++ ('Programa de operações aritméticas!!!!!.py
@@ -0,0 +1,28 @@
+print('Programa de operações aritméticas!!!!!!!!!!!!!!')
+x = int(input('Digite o primeiro número: '))
+y = int(input('Digite o segundo número: '))
+
+soma = (x + y)
+print('A soma é: ', soma) 
+
+sub = (x - y)
+print('A subtração é: ', sub) 
+
+mult = (x * y)
+print('A multiplicação é: ', mult) 
+
+div = (x / y)
+print('A divisão é: ', div)
+
+resto = (x % y)
+print('O resto da divisão entre os dois números é: ', resto)
+
+exp = (x ** y)
+print('O número o primeiro número elevado ao segundo é: ', exp)
+
+divExata = (x // y)
+print('A divisão exata entre os números é: ', divExata)
+
+
+
+
