a902e6e553ebd2f2893b52ec8be4c881997ec18a
luiz <luizfk.marinho@gmail.com>
2024-03-15 21:18:56 -0300

    02 modificado

--- 02.py
+++ 02.py
@@ -2,6 +2,6 @@ a=int(input("digite seu ano de nascimento"))
 b=int(input("digite o ano atual"))
 idade=b-a
 if idade>=18:
-    print("você é maior de idade")
+    print("Você tem", idade, "é maior de idade")
 else:
-    print("você não é maior de idade")
+    print("Você tem", idade, "é menor de idade")
